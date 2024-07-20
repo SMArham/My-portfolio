8ef50c444e2e101e69833166b1f9c13e3904b151
Syed Muhammad Arham <syedarhamreal@gmail.com>
2024-07-21 01:12:15 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('nav ul li a');
+
+window.onscroll = () => {
+    let scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+    sections.forEach(section => {
+        let sectionTop = section.offsetTop - 150;
+        let sectionHeight = section.offsetHeight;
+        let sectionId = section.getAttribute('id');
+
+        if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                if (link.getAttribute('href') === `#${sectionId}`) {
+                    link.classList.add('active');
+                }
+            });
+        }
+    });
+};
